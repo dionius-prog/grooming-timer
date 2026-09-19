1493bc71ca69f267e047094097b2f5521d2d2a32
dionius-prog <d.i.o.n.i.u.s@list.ru>
2026-09-20 00:22:25 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => self.clients.claim());
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.open('groom-v3').then(c =>
+    caches.open('groom-v4').then(c =>
       c.match(e.request).then(r => r || fetch(e.request).then(resp => {
         c.put(e.request, resp.clone());
         return resp;
